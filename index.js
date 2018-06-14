1019ee58a5bf8cdebbae8c007be5f3190f9b2b22
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-06-14 14:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ function currentLine(x) {
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+
 }
+
+function takeANumber2(katDeliLine){
+  
+  katzDeliLine.push()
+  return "Welcome, you are "
+  
+  
+  
+}
+
 function nowServing(x) {
   if (x.length === 0) {
     return "There is nobody waiting to be served!"
